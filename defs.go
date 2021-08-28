6593b7a4a0ea85a36ade45c84373c00e34f79f13
Kamiar Bahri <kbahri@hotmail.com>
2021-08-28 13:35:59 +0100

    fixed spelling in defs.go

--- defs.go
+++ defs.go
@@ -4,7 +4,7 @@ type messageBanner struct {
 	On               bool `json:"on"`
 	SecondsToDisplay int  `json:"seconds-to-display"`
 
-	// When the value of On chnages from false to true
+	// When the value of On changes from false to true
 	// Tickout is set to SecondsToDisplay and then
 	// decremented every second until the banner is closed
 	// (on set to false).
@@ -16,7 +16,7 @@ type httpx struct {
 }
 
 // tlsFiles defines the location of the certificate and
-// private-key fiels. Both files must be
+// private-key files. Both files must be
 // in PEM format.
 type tlsFiles struct {
 	CertFilePath string `json:"cert-file-path"`
@@ -28,14 +28,14 @@ type urlPaths struct {
 	Exclude  []string `json:"exclude"`
 }
 
-// admin defines the ip addresses
-// of machins that connect to the server.
+// admin defines the IP addresses
+// of machines that connect to the server.
 // The admin pages (or app), whether hosted inside
-// the public site or in a separete environment
+// the public site or in a separate environment
 // should be secured as such that it would be only
 // available via the localhost (i.e. on the local
 // machine or ssh tunnel) or a list of recognized
-// ip addresses.
+// IP addresses.
 type admin struct {
 	RunOnStartup bool     `json:"run-on-sartup"`
 	PortNo       uint     `json:"port-no"`
@@ -45,7 +45,7 @@ type admin struct {
 // siteStats holds the basic stat that can be
 // set in connstat. Every server can create one,
 // and handle connections accordingly; the active,
-// idel, and new states must be set and udated by
+// idel, and new states must be set and undated by
 // the web server.
 type siteStats struct {
 	Active uint `json:"active"`
@@ -59,7 +59,7 @@ type site struct {
 	PortNo   int    `json:"portno"`
 }
 
-// Config is defines the fields that are typicaly required for
+// Config is defines the fields that are typically required for
 // web configuration.  All config values have to have a
 // presentation in this struct.
 type Config struct {
@@ -74,8 +74,8 @@ type Config struct {
 	Site               site      `json:"site"`
 	URLPaths           urlPaths  `json:"url-paths"`
 
-	// These are the offender ip addr. Their connections
-	// are drop immedietely, without any message returned to them.
+	// These are the offender IP addr. Their connections
+	// are drop immediately, without any message returned to them.
 	BlockedIP []string `json:"blocked-ip"`
 
 	RedirectHTTPtoHTTPS bool `json:"redirect-http-to-https"`
@@ -101,7 +101,7 @@ const (
 # About this config
 #   --delimiter between key and value is space (one or many).
 #   --comments must begin with #.
-#   --one key/valuee per line; to continue to another line, 
+#   --one key/value per line; to continue to another line, 
 #     place a backslash (\) at the end of the statement.
 #   --The headers and keys are case- insensitive, but the
 #     following is the recommanded format:
@@ -109,7 +109,7 @@ const (
 #           key_1  value_1
 #     All entries can appear with or without a header.      
 #    
-#   Some of the config values are directely related to implementation
+#   Some of the config values are directly related to implementation
 #   of feature within a Go website. Please, see the following template
 #   for implementation of these feature.
 #   https://github.com/kambahr/go-webstandard
@@ -117,7 +117,7 @@ const (
 
 # This is the hostname that will be accessed from the outside
 # i.e. mydomain.com. It may still be localhost if the website is
-# on a loadbalancer.
+# on a load balancer.
 Site
 	hostname         localhost
 	portno           8085
@@ -126,7 +126,7 @@ Site
 # location of certificate and private files;
 # both in the PEM format and must be full path.
 # The paths can be an local paths; but 
-# /appdata/certs/<domain anme>/ is recommended.
+# /appdata/certs/<domain name>/ is recommended.
 TLS 
    #cert /usr/local/mywebapp dir/appdata/certs/mydomain/certx.pem
    #key /usr/local/webapp dir/appdata/certs/mydomain/keyx.pem
@@ -137,7 +137,7 @@ TLS
 maintenance-window     off
  
 Admin
-   # List of ip address that will be allowed to access 
+   # List of IP address that will be allowed to access 
    # the admin website; separated by comma; otherwise, the admin
    # section of the website will only be served to the local machine.
    allowed-ip-addr	<ip add 1>, <ip add 2>
@@ -156,14 +156,14 @@ MessageBanner
    display-mode      off
 
    # The banner message can be displayed for the below indicated value;
-   # and then be automatically disalbed (display-mode => off), when this 
+   # and then be automatically disabled (display-mode => off), when this 
    # period elapses:
-   #    display-mode will be seto off from on.
+   #    display-mode will be set to off from on.
    # A of value > 0 will trigger the auto-timeout. So, if you would like
    # to clear the banner message manually keep this value set to zero.
    seconds-to-display  0
 
-# URL paths can be restried, exluded and forwarded explicitly; the end-user 
+# URL paths can be restricted, excluded and forwarded explicitly; the end-user 
 # will receive the appropriate error message.
 # These option to make a portion of your site unavailable for maintenance
 # or other reasons. Each path must begin with a slash (relative path).
@@ -172,10 +172,10 @@ MessageBanner
 URLPaths
    # restrict-paths <url paths separated by comma>
    # e.g.
-   # restrict-paths   /gallery,/accouting, /customer-review, /myblog
+   # restrict-paths   /gallery,/accounting, /customer-review, /myblog
 
    # with the exclude option, files will be intact in the same location, but not
-   # served; the end-user will receive 404 error; or the behavior can be 
+   # served; the end-user will receive 404 error; or the behaviour can be 
    # customized.
    exclude-paths /galleryx, /someotherpath
 
@@ -207,6 +207,6 @@ Data
 # the following is the format:
 # <ip address><minimum of one space><description>
 # Example:
-# 10.12.3.4 <a short descrription of the reason>
+# 10.12.3.4 <a short description of the reason>
 `
 )
